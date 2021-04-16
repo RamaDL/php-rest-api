7348da8072d090f4d35458f86b9476f72ef37b43
rdutto <rdutto@kafesistemas.com.ar>
2021-04-16 12:11:47 -0300

    ADD php configuration. ADD php dependecies. ADD DB script.sql.

--- bootstrap.php
+++ bootstrap.php
@@ -3,8 +3,3 @@ require 'vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-
-// test code, should output:
-// api://default
-// when you run $ php bootstrap.php
-echo $_ENV['OKTAAUDIENCE'];
